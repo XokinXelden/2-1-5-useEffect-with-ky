8a19147853e61d3a0364cc2e4c6cd8d65fb18107
XokinXelden <xokinxelden@gmail.com>
2025-09-17 22:37:16 +0300

    каааа

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -11,21 +11,27 @@ type Post = {
 
 function App() {
   const [posts, setPosts] = useState<Post[]>([]);
+  const [loading,setLoading] = useState<boolean>(true)
 
   useEffect(() => {
     const getPosts = async () => {
+      try{
       const data: Post[] = await ky
         .get("https://jsonplaceholder.typicode.com/posts", { timeout: 20000 })
         .json<Post[]>();
       setPosts(data);
-      console.log(posts);
+      }catch(err){
+        console.log(err.message)
+      }finally{
+        setLoading(false)
+      }
     };
     const intervalGetPost = setInterval(getPosts, 5000);
     return () => {
       clearInterval(intervalGetPost);
     };
-  });
-  if (posts.length === 0) return <h1>Loading...</h1>;
+  },[]);
+  if (loading) return <h1>Loading...</h1>;
   return (
     <>
       <h1>Posts</h1>
